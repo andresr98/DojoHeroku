0e628d38ebc740353884c141f6785c31172ed0c6
Andres <aandres.r98@gmail.com>
2018-09-22 11:39:16 -0500

    Reading Query Params

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 		<title> Pruba de PHP y Heroku </title>
 	</head>
 	<body>
-		<?php echo'<p>Hola' . htmlspecialchars($_GET["name"]) . '</p>';?>
+		<?php echo'<p>Saludos ' . htmlspecialchars($_GET["name"]) . '</p>';?>
 	</body>
 </html>
